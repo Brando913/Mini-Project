0be4ad65cccc0e2564f55f695f3a7da42cea154a
Brandon Alvarez <brandonalvarex15@gmail.com>
2025-12-03 15:53:27 -0600

    only comments

--- SortAlgo.py
+++ SortAlgo.py
@@ -1,6 +1,14 @@
 import time
 import random
 
+#small array for algorithms to sort
 SmallArray = [3, 8, 12, 5, 19, 7, 2, 14, 9, 11]
 
+#large array for alorithms to sort
 LargeArray = [random.randint(0, 1000) for _ in range(10000)]
+
+#bubble sort
+
+#insertion sort
+
+#merge sort
